e3418973241e2f259f185a4a17a70686523bff64
williambl <william@williambl.com>
2023-02-20 18:55:28 +0000

    feat: add /set_block endpoint

--- src/main/java/com/williambl/craftttp/CraftTTP.java
+++ src/main/java/com/williambl/craftttp/CraftTTP.java
@@ -1,15 +1,21 @@
 package com.williambl.craftttp;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.sun.net.httpserver.HttpServer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.commands.arguments.blocks.BlockStateParser;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -28,6 +34,12 @@ public class CraftTTP implements ModInitializer {
             try {
                 var httpServer = HttpServer.create(new InetSocketAddress(8394), 0);
                 httpServer.createContext("/get_block", httpExchange -> {
+                    if (!httpExchange.getRequestMethod().equals("GET")) {
+                        httpExchange.sendResponseHeaders(405, -1);
+                        httpExchange.getResponseBody().close();
+                        return;
+                    }
+
                     Map<String, String> queryParams = getQueryParams(httpExchange.getRequestURI().getRawQuery());
                     if (!queryParams.containsKey("x") || !queryParams.containsKey("y") || !queryParams.containsKey("z")) {
                         httpExchange.sendResponseHeaders(400, -1);
@@ -46,6 +58,48 @@ public class CraftTTP implements ModInitializer {
                     }
 
                     var state = server.submit(() -> server.overworld().getBlockState(new BlockPos(x, y, z))).join();
+
+                    String response = BlockStateParser.serialize(state);
+                    httpExchange.sendResponseHeaders(200, response.length());
+                    try (var os = httpExchange.getResponseBody()) {
+                        os.write(response.getBytes());
+                    }
+                });
+                httpServer.createContext("/set_block", httpExchange -> {
+                    if (!httpExchange.getRequestMethod().equals("PUT")) {
+                        httpExchange.sendResponseHeaders(405, -1);
+                        httpExchange.getResponseBody().close();
+                        return;
+                    }
+
+                    Map<String, String> queryParams = getQueryParams(httpExchange.getRequestURI().getRawQuery());
+                    if (!queryParams.containsKey("x") || !queryParams.containsKey("y") || !queryParams.containsKey("z")) {
+                        httpExchange.sendResponseHeaders(400, -1);
+                        httpExchange.getResponseBody().close();
+                        return;
+                    }
+                    int x, y, z;
+                    try {
+                        x = Integer.parseInt(queryParams.get("x"));
+                        y = Integer.parseInt(queryParams.get("y"));
+                        z = Integer.parseInt(queryParams.get("z"));
+                    } catch (NumberFormatException e) {
+                        httpExchange.sendResponseHeaders(400, -1);
+                        httpExchange.getResponseBody().close();
+                        return;
+                    }
+
+                    BlockStateParser.BlockResult state;
+                    try (var is = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody()))) {
+                        String stateStr = is.readLine();
+                        state = BlockStateParser.parseForBlock(server.overworld().holderLookup(Registries.BLOCK), stateStr, true);
+                    } catch (CommandSyntaxException e) {
+                        httpExchange.sendResponseHeaders(400, -1);
+                        httpExchange.getResponseBody().close();
+                        return;
+                    }
+
+                    server.execute(() -> server.overworld().setBlockAndUpdate(new BlockPos(x, y, z), state.blockState()));
 
                     String response = state.toString();
                     httpExchange.sendResponseHeaders(200, response.length());
