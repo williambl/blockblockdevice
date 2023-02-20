02e041cc2ceaa7b318d07f47d8e127bf9db3c972
williambl <william@williambl.com>
2023-02-20 20:43:02 +0000

    feat: /generate_memory command

--- src/main/java/com/williambl/craftttp/CraftTTP.java
+++ src/main/java/com/williambl/craftttp/CraftTTP.java
@@ -1,15 +1,31 @@
 package com.williambl.craftttp;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.blocks.BlockStateParser;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+import net.minecraft.world.level.levelgen.Heightmap;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +40,8 @@ import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static net.minecraft.commands.Commands.*;
 
 public class CraftTTP implements ModInitializer {
     private static final WeakHashMap<MinecraftServer, HttpServer> SERVERS = new WeakHashMap<>();
@@ -86,6 +104,62 @@ public class CraftTTP implements ModInitializer {
                 LOGGER.info("Stopped CraftTTP server.");
             }
         });
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(
+                literal("generate_memory").executes(ctx -> {
+                    ChunkPos chunkPos = new ChunkPos(new BlockPos(ctx.getSource().getPosition()));
+                    ServerLevel level = ctx.getSource().getLevel();
+                    LevelChunk chunk = level.getChunk(chunkPos.x, chunkPos.z);
+                    int minY = level.getMinBuildHeight();
+                    int maxY = level.getMaxBuildHeight();
+                    LevelChunkSection[] sections = chunk.getSections();
+                    int i = 0;
+                    for (int y = minY; y < maxY; y++) {
+                        LevelChunkSection section = sections[level.getSectionIndex(y)];
+                        if (section == null) {
+                            section = new LevelChunkSection(y, level.registryAccess().registryOrThrow(Registries.BIOME));
+                            sections[level.getSectionIndex(y)] = section;
+                        }
+                        if (y == minY) {
+                            for (int x = 0; x < 16; x++) for (int z = 0; z < 16; z++) {
+                                section.setBlockState(x, y & 15, z, Blocks.WHITE_WOOL.defaultBlockState());
+                            }
+                        } else {
+                            for (int z = 0; z < 16; z++) {
+                                BlockState state = switch (z % 4) {
+                                    case 0 -> Blocks.REDSTONE_WALL_TORCH.defaultBlockState().setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.NORTH).setValue(BlockStateProperties.LIT, false);
+                                    case 1 -> Blocks.WHITE_WOOL.defaultBlockState();
+                                    case 2 -> Blocks.LEVER.defaultBlockState().setValue(BlockStateProperties.ATTACH_FACE, AttachFace.WALL).setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.SOUTH).setValue(BlockStateProperties.POWERED, true);
+                                    case 3 -> Blocks.SCAFFOLDING.defaultBlockState().setValue(BlockStateProperties.STABILITY_DISTANCE, 0);
+                                    default -> throw new IllegalStateException("Unexpected value: " + z % 4);
+                                };
+                                if (state.getBlock() == Blocks.REDSTONE_WALL_TORCH) {
+                                    i++;
+                                }
+                                for (int x = 0; x < 16; x++) {
+                                    section.setBlockState(x, y & 15, z, state);
+                                }
+                            }
+                        }
+                    }
+
+                    for (var section : sections) for (int x = 0; x < 16; x++) for (int z = 0; z < 16; z++) {
+                        level.getChunkSource().getLightEngine().updateSectionStatus(chunkPos.getBlockAt(x, section.bottomBlockY(), z), false);
+                    }
+
+                    for (int x = 0; x < 16; x++) for (int z = 0; z < 16; z++) for (var type : Heightmap.Types.values()) {
+                        chunk.getOrCreateHeightmapUnprimed(type).update(x, maxY, z, chunk.getBlockState(chunkPos.getBlockAt(x, maxY, z)));
+                    }
+
+                    chunk.clearAllBlockEntities();
+                    chunk.setUnsaved(true);
+                    level.getChunkSource().chunkMap.resendChunk(chunk);
+
+                    ctx.getSource().sendSuccess(Component.literal("Created a block of %s bits (%s bytes) of memory.".formatted(i, i/8)), true);
+
+                    return Command.SINGLE_SUCCESS;
+                })
+        ));
     }
 
     private static Map<String, String> getQueryParams(@Nullable String rawQueryString) {
